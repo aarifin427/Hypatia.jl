040c6db6b0c3cd6db0e51ed0572766763a6c8861
aarifin427 <audriana.arifin@gmail.com>
2022-05-06 03:09:44 +1000

    custom routine example

--- workspace/diff.jl
+++ workspace/diff.jl
@@ -25,3 +25,34 @@ f(x) = x[1]^2 + x[2]^2 + x[3]^2
 
 g = x -> ForwardDiff.gradient(f, x);
 g([1,2,3])
+
+# ===========================================================#
+"""
+ref: http://www.cecm.sfu.ca/~mmonagan/talks/eccad2013.pdf
+"""
+# ===========================================================#
+
+p = [     # data structure representing a polynomial
+    1 1 1 # the coefficients of every term
+    2 0 0 # power of x in every term
+    0 2 0 # power of y in every term
+    0 0 2 # power of z in every term
+]
+
+# TODO: conventional function documentation in Julia
+function gradient(point, f)
+    """
+    Function: outputs the gradient value of polynomial f.
+    Currently only works with monomials(?) (only works with x^a + y^b, not x^a*y^b + x^c*y^d)
+        f       : polynomial function represented as a matrix
+        point   : in this example, it's 3D, representing (x,y,z)
+    """
+    res = []
+    _, y = size(f)
+    for i in 1:y
+        append!(res, f[1,i]*f[i+1,i]*point[i]^(f[i+1,i] - 1))
+    end;
+    return res
+end;
+
+@assert g([1,2,3]) == gradient([1,2,3], p)
