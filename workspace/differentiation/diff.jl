e0cb00befb248f3e50eb051b8a4250a1e09df70c
aarifin427 <audriana.arifin@gmail.com>
2022-05-13 21:08:15 +1000

    grad and hess product subroutines, tests passed

--- workspace/differentiation/diff.jl
+++ workspace/differentiation/diff.jl
@@ -16,7 +16,7 @@ ref: http://www.cecm.sfu.ca/~mmonagan/talks/eccad2013.pdf
 # =====================================================#
 # Function representation to value
 # =====================================================#
-function func_val(p::Matrix{T}, x::Vector{T}) where {T <: Real}
+function func_eval(p::Matrix{T}, x::Vector{T}) where {T <: Real}
     res = 0 # result
     (row, col) = size(p)
     for j = 1:col
@@ -30,20 +30,68 @@ function func_val(p::Matrix{T}, x::Vector{T}) where {T <: Real}
 end
 
 # =====================================================#
-# Gradient function
+# Matrix representation to value
 # =====================================================#
-# TODO: conventional function documentation in Julia
-function gradient(point, f)
-    """
-    Function: outputs the gradient value of polynomial f.
-    Currently only works with monomials(?) (only works with x^a + y^b, not x^a*y^b + x^c*y^d)
-        f       : polynomial function represented as a matrix
-        point   : in this example, it's 3D, representing (x,y,z)
-    """
-    res = []
-    _, y = size(f)
-    for i in 1:y
-        append!(res, f[1,i]*f[i+1,i]*point[i]^(f[i+1,i] - 1))
-    end;
+function mat_eval(amat::Matrix{Matrix{T}}, x::Vector{T}) where {T <: Real}
+    row, col = size(amat)
+    if col == 1
+        res = Vector{T}(undef, row)
+    else
+        res = Matrix{T}(undef, row, col)
+    end
+    for i = 1:row
+        for j = 1:col
+            res[i,j] = func_eval(amat[i,j], x)
+        end
+    end
     return res
-end;
+end
+# TODO: test this matrix val Function
+
+# =====================================================#
+# Partially differentiate
+# =====================================================#
+function partial_diff(p::Matrix{T}, idx::Int) where {T <: Real}
+    """
+    Get partial derivative of p(x) in terms of x_idx or the idx-th variable.
+    """
+    row, col = size(p)
+    res = Matrix{T}(undef, row, 1)
+    for j = 1:col
+        if p[idx+1, j] == 0
+            continue
+        end
+        vec = p[:, j]
+        vec[1,1] *= vec[idx+1, 1]
+        vec[idx+1, 1] -= 1
+        res = hcat(res, vec)
+    end
+    return res[:, 2:end]
+end
+
+# =====================================================#
+# Get grad matrix function rep
+# =====================================================#
+function compute_grad_rep(p::Matrix{T}) where {T <: Real}
+    row, _ = size(p)
+    dp = Matrix{Matrix{T}}(undef, row-1, 1)
+    for i = 1:row-1
+        dp[i,1] = partial_diff(p, i)
+    end
+    return dp
+end
+
+# # =====================================================#
+# # Get jacobian matrix function rep
+# # =====================================================#
+function compute_hess_rep(dp::Matrix{Matrix{T}}) where {T <: Real}
+    row, _ = size(dp)
+    ddp = Matrix{Matrix{T}}(undef, row, row)
+    for i = 1:row
+        for j = 1:row
+            ddp[i,j] = partial_diff(dp[i,1], j)
+        end
+    end
+    return ddp
+end
+# TODO: Test
--- workspace/differentiation/test.jl
+++ workspace/differentiation/test.jl
@@ -4,62 +4,132 @@ using Random
 include("diff.jl")
 include("function_examples.jl")
 
+@testset "Partial diff" begin
+    @test partial_diff(p2, 1) == [
+        2 2 -8 4
+        1 1 0 1
+        2 0 1 0
+        0 2 1 0
+        0 0 1 2
+    ]
+end
+
+@testset "Compute Grad representation" begin
+    dx1 = [
+        2 2 -8 4
+        1 1 0 1
+        2 0 1 0
+        0 2 1 0
+        0 0 1 2
+    ]
+    dx2 = [
+        2 2 -8 4
+        2 0 1 0
+        1 1 0 1
+        0 2 1 0
+        0 0 1 2
+    ]
+    dx3 = [
+        2 2 4 -8
+        2 0 0 1
+        0 2 0 1
+        1 1 3 0
+        0 0 0 1
+    ]
+    dx4 = [
+        -8 4 4
+        1 2 0
+        1 0 2
+        1 0 0
+        0 1 1
+    ]
+    x1, x2, x3, x4 = compute_grad_rep(p2)
+    @test [x1, x2, x3, x4] == [dx1, dx2, dx3, dx4]
+end
+
+@testset "Matrix Representation to Value" begin
+    @testset "Polynomial value" begin
+        @testset "Function 1" begin
+            for k = 1:100
+                local point = [rand(1:10), rand(1:10), rand(1:10), rand(1:10)]
+                @test f1(point) == func_eval(p1, point)
+            end
+        end
+        @testset "Function 2" begin
+            for k = 1:100
+                local point = [rand(1:10), rand(1:10), rand(1:10), rand(1:10)]
+                @test f2(point) == func_eval(p2, point)
+            end
+        end
+        
+    end
+    @testset "Gradient" begin
+        # function 1
+        @testset "Function 1" begin
+            local f(x) = f1(x)
+            local p = p1
+            local g = x -> ForwardDiff.gradient(f, x);
+
+            for k = 1:100
+                local point = [rand(1:10), rand(1:10), rand(1:10), rand(1:10)]
+                res = g(point) .== mat_eval(compute_grad_rep(p), point)
+                @test sum(res) == length(res)
+            end
+        end
+        # function 2
+        @testset "Function 2" begin
+            local f(x) = f2(x)
+            local p = p2
+            local g = x -> ForwardDiff.gradient(f, x);
+
+            for k = 1:100
+                local point = [rand(1:10), rand(1:10), rand(1:10), rand(1:10)]
+                res = g(point) .== mat_eval(compute_grad_rep(p), point)
+                @test sum(res) == length(res)
+            end
+        end
+    end
+    @testset "Hessian" begin
+        # function 1
+        @testset "Function 1" begin
+            local f(x) = f1(x)
+            local p = p1
+            local g = x -> ForwardDiff.hessian(f, x)
+
+            for k = 1:100
+                local point = [rand(1:10), rand(1:10), rand(1:10), rand(1:10)]
+                hess = compute_hess_rep(compute_grad_rep(p))
+                res = mat_eval(hess, point)
+                @test g(point) == res
+            end
+        end
+        # function 2
+        @testset "Function 2" begin
+            local f(x) = f2(x)
+            local p = p2
+            local g = x -> ForwardDiff.hessian(f, x);
+
+            for k = 1:100
+                local point = [rand(1:10), rand(1:10), rand(1:10), rand(1:10)]
+                hess = compute_hess_rep(compute_grad_rep(p))
+                res = mat_eval(hess, point)
+                @test g(point) == res
+            end
+        end
+    end
+end
+
 @testset "Substitute values into polynomial function representations" begin
     @testset "Function 1: Vamos polynomial" begin
         for k = 1:100
             local point = [rand(1:10), rand(1:10), rand(1:10), rand(1:10)]
-            @test f1(point) == func_val(p1, point)
+            @test f1(point) == func_eval(p1, point)
         end
     end
     @testset "Function 2: Polynomial W from Blekherman" begin
         for k = 1:100
             local point = [rand(1:10), rand(1:10), rand(1:10), rand(1:10)]
-            @test f2(point) == func_val(p2, point)
+            @test f2(point) == func_eval(p2, point)
         end
-    end
-end
-
-@testset "y = x^a + y^b + z^c structure" begin
-    @testset "Polynomial of 4 terms" begin
-        pwc = 50 # power cap
-        n = 100  # number of tests
-        for k = 1:n
-            local a, b, c = rand(1:pwc), rand(1:pwc), rand(1:pwc)
-            local p = [
-                rand(1:pwc) rand(1:pwc) rand(1:pwc)
-                rand(1:pwc) 0           0          
-                0           rand(1:pwc) 0          
-                0           0           rand(1:pwc)
-            ]
-            local f(x) = p[1,1]*x[1]^p[2,1] + p[1,2]*x[2]^p[3,2] + p[1,3]*x[3]^p[4,3]
-            local g = x -> ForwardDiff.gradient(f, x);
-            local h = x -> ForwardDiff.hessian(f, x);
-            local point = [rand(1:10), rand(1:10), rand(1:10)]
-            
-            @test g(point) == gradient(point, p)
-            # @test h(point) == hessian(point, p)
-        end;
-    end
-
-    @testset "Polynomial of 5 terms" begin
-        pwc = 50 # power cap
-        n = 100  # number of tests
-        for k = 1:n
-            local a, b, c = rand(1:pwc), rand(1:pwc), rand(1:pwc)
-            local p = [
-                rand(1:pwc) rand(1:pwc) rand(1:pwc) rand(1:pwc)
-                rand(1:pwc) 0           0           0
-                0           rand(1:pwc) 0           0
-                0           0           rand(1:pwc) 0
-                0           0           0           rand(1:pwc)
-            ]
-            local f(x) = p[1,1]*x[1]^p[2,1] + p[1,2]*x[2]^p[3,2] + p[1,3]*x[3]^p[4,3] + p[1,4]*x[4]^p[5,4]
-            local g = x -> ForwardDiff.gradient(f, x);
-            local h = x -> ForwardDiff.hessian(f, x);
-            local point = [rand(1:10), rand(1:10), rand(1:10), rand(1:10)]
-            
-            @test g(point) == gradient(point, p)
-            # @test h(point) == hessian(point, p)
-        end;
     end
 end
